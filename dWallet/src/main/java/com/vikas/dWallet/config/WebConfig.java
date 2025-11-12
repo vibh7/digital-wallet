b2b71864f39c0e7fe818c7ec1795b89aeb9690ca
vikas <vikasbhartilm10@gmail.com>
2025-11-12 23:16:08 +0530

    optimized-cors

--- dWallet/src/main/java/com/vikas/dWallet/config/WebConfig.java
+++ dWallet/src/main/java/com/vikas/dWallet/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-          .allowedOrigins("http://localhost:5173")
+          .allowedOrigins("http://localhost:5173","https://digital-wallet-psi.vercel.app")
           .allowedMethods("*")
           .allowedHeaders("*")
           .allowCredentials(true);
